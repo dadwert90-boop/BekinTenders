9f8862efd479aff0f69c08be4115cd6c9ce83630
Sandura Shumba <sandurashumba@Sanduras-MacBook-Pro.local>
2026-02-14 18:11:06 +0200

    Configure Flask run port

--- app.py
+++ app.py
@@ -1460,4 +1460,5 @@ def datetime_fmt(value):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
